1a5715511b6acc95b07b6e5c81c1e04b1ff026fc
Febri Hidayan <febrihidayan@gmail.com>
2021-08-01 16:00:42 +0700

    change svg icon with Heroicons

--- resources/js/components/modules/BoxAside.vue
+++ resources/js/components/modules/BoxAside.vue
@@ -7,9 +7,9 @@
     >
         <div class="flex bg-blue-500 dark:bg-dark-400 text-white p-4">
             <a class="cursor-pointer" @click="isActive=false">
-                <svg class="svg-icon !fill-white !stroke-white" viewBox="0 0 20 20">
-                    <path d="M8.388,10.049l4.76-4.873c0.303-0.31,0.297-0.804-0.012-1.105c-0.309-0.304-0.803-0.293-1.105,0.012L6.726,9.516c-0.303,0.31-0.296,0.805,0.012,1.105l5.433,5.307c0.152,0.148,0.35,0.223,0.547,0.223c0.203,0,0.406-0.08,0.559-0.236c0.303-0.309,0.295-0.803-0.012-1.104L8.388,10.049z"></path>
-                </svg>
+                <ChevronLeftIcon
+                    class="svg-icon !text-white"
+                />
             </a>
             <p class="text-base ml-3">{{
                 name
@@ -22,11 +22,14 @@
 </template>
 
 <script>
+import { ChevronLeftIcon } from '@heroicons/vue/outline'
+
 export default {
     props: {
         modelValue: Boolean,
         name: String
     },
+    components: { ChevronLeftIcon },
     data() {
         return {
             isActive: this.modelValue || false
